a459d40a87490d574f1eca2debdd05048c9443ed
Mitró György Krisztián <mitrogyuri0421@gmail.com>
2023-11-17 19:26:02 +0100

    basic terminate command override

---
+++ final/ticket-service/src/main/java/com/epam/training/ticketservice/ui/AppCommand.java
@@ -0,0 +1,14 @@
+package com.epam.training.ticketservice.ui;
+
+import org.springframework.shell.standard.ShellComponent;
+import org.springframework.shell.standard.ShellMethod;
+import org.springframework.shell.standard.commands.Quit;
+
+@ShellComponent
+public class AppCommand implements Quit.Command{
+
+    @ShellMethod(key = "exit", value = "terminates the program")
+    public void terminate(){
+        System.exit(0);
+    }
+}
